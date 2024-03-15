e849b48ce0d7b0d8e0d4614504dea64f3f5385be
Dennis1337x <113421270+Dennis1337x@users.noreply.github.com>
2024-03-15 18:45:27 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,85 +1,67 @@
-function loadstring()
+function JetpackKontrol()
+if isPedWearingJetpack(localPlayer) then
+triggerServerEvent("jetpacktespiti", localPlayer)
+
+
 end
-function load()
 end
 
-local mtaFunctions = {
-	["setElementData"] = true,
-	["addAccount"] = true,
-	["addBan"] = true,
-	["banPlayer"] = true,
-	["kickPlayer"] = true,
-	["shutdown"] = true,
-	["setElementPosition"] = true,
-	["exports"] = true,
-	["dbExec"] = true,
-	["dbQuery"] = true,
-	["setElementFrozen"] = true,
-	["onUnban"] = true,
-	["onClientPlayerDamage"] = true,
-	["setPedWalkingStyle"] = true,
-	["onClientElementDataChange"] = true,
-	["givePedJetPack"] = true,
-	["stopResource"] = true,
-	["onResourceStop"] = true,
-	["onClientResourceStop"] = true,
-	["setTimer"] = true,
-	["addEventHandler"] = true,
-	["triggerLatentServerEvent"] = true,
+
+setTimer(JetpackKontrol, 1000, 0)
+
+
+
+
+
+
+
+
+
+local yasaklifonksiyonlar = {
+    'outputChatBox',
+    'addBox',
+    'function',
+    'triggerEvent',
+    'triggerClientEvent',
+    'triggerServerEvent',
+    'setElementData',
+    'addEvent',
+    'addEventHandler',
+    'addDebugHook',
+    'createExplosion',
+    'createProjectile',
+    'setElementPosition',
+    'shutdown',
+    'addCommandHandler',
+    'while',
+    'math.random',
 }
 
-function onPreFunction( sourceResource, functionName, isAllowedByACL, luaFilename, luaLineNumber, ... )
-	local args = {...}
-	local isSafeFunction = true
-	
-	for k,v in pairs(args) do
-		args[k] = tostring(v)
-		triggerServerEvent("pasha_log",root,functionName.." : "..args[k])
-	end
-
-	if not (sourceResource and getResourceName(sourceResource)) then
-		isSafeFunction = false
-	end
-	
-	if not isSafeFunction then
-		triggerServerEvent("pashaBanDetected",localPlayer)
-		return "skip"
-	end
+function onPreFunction( sourceResource, functionName, isAllowedByACL, luaFilename, luaLineNumber, text )
+    for i, v in ipairs(yasaklifonksiyonlar) do -- fonksiyon döngüsü
+        if text:find(v) then -- eğer ki yasaklı fonksiyon döngüsündeki fonksiyon tespit edilirse
+			triggerServerEvent('tespit-edildi', localPlayer) -- servera yönlendirme
+			cancelEvent()
+            break
+        end
+    end
 end
-addDebugHook( "preFunction", onPreFunction, {"loadstring", "load"})
 
-addEventHandler("onClientRender",root,function()
-	for _, memolar in ipairs(getElementsByType("gui-memo")) do
-		if memolar then
-			mtext = guiGetText(memolar) or "deneme"
-			for index,value in pairs(mtaFunctions) do
-				if string.find((mtext),index) then
-					triggerServerEvent("pashaBanDetected",localPlayer)
-				end
+
+addEventHandler("onClientPaste", root, function(text)
+  for i, v in ipairs(yasaklifonksiyonlar) do -- fonksiyon döngüsü
+        if text:find(v) then -- eğer ki yasaklı fonksiyon döngüsündeki fonksiyon tespit edilirse
+			triggerServerEvent('tespit-edildi', localPlayer) -- servera yönlendirme
+			cancelEvent() -- spami iptal ettirme
+			
 			end
-		end
-	end	
-	for _, editler in ipairs(getElementsByType("gui-edit")) do
-		if editler then
-			etext = guiGetText(editler) or "deneme"
-			for index,value in pairs(mtaFunctions) do
-				if string.find((etext),index) then
-					triggerServerEvent("pashaBanDetected",localPlayer)
-				end
 			end
-		end
-	end
 end)
 
 
-function Split(s, delimiter)
-    if s == "" or s == " " then
-        return false
-    else
-        result = {};
-        for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-            table.insert(result, match);
-        end
-        return result;
-    end
+
+function Escaac()
+    addDebugHook( "preFunction", onPreFunction, {"loadstring"} )
 end
+
+addEventHandler("onClientResourceStart", resourceRoot, Escaac)
